41b309e92439fc3565b11b211c6ece1f874669e2
Georges Haidar <ghaidar0@gmail.com>
2021-09-27 00:24:20 +0100

    neovim: configure telescope

--- neovim/.config/nvim/plugin/telescope.lua
+++ neovim/.config/nvim/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require('telescope')
+local actions = require('telescope.actions')
 
 local mapopts = { silent = true, noremap = true }
 
@@ -7,14 +8,56 @@ vim.api.nvim_set_keymap('n', '<Leader>o', '<cmd>lua require("telescope.builtin")
 vim.api.nvim_set_keymap('n', '<Leader>s', '<cmd>lua require("telescope.builtin").live_grep()<cr>', mapopts)
 
 telescope.setup({
+	defaults = {
+	 mappings = {
+		i = {
+			["<esc>"] = actions.close,
+			["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			["<C-a>"] = actions.send_to_qflist + actions.open_qflist,
+		},
+		n = {
+			["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			["<C-a>"] = actions.send_to_qflist + actions.open_qflist,
+		}
+	 },
+	},
+	pickers = {
+		find_files = {
+			find_command = {'rg', '--files', '--ignore', '--hidden'},
+		},
+		live_grep = {
+			theme = 'ivy',
+		},
+		lsp_references = {
+			theme = 'ivy',
+		},
+		lsp_implementations = {
+			theme = 'ivy'
+		},
+		lsp_definitions = {
+			theme = 'ivy'
+		},
+		lsp_type_definitions = {
+			theme = 'ivy'
+		},
+		lsp_code_actions = {
+			theme = 'cursor',
+		},
+		lsp_range_code_actions = {
+			theme = 'cursor',
+		},
+		lsp_document_diagnostics = {
+			theme = 'ivy',
+		},
+	},
 	extensions = {
 		fzf = {
 			fuzzy = true,
 			override_generic_sorter = true,
 			override_file_sorter = true,
 			case_mode = "smart_case",
-		}
-	}
+		},
+	},
 })
 
 telescope.load_extension('fzf')
